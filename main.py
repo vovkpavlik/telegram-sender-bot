64851ba8a5a3df94c17d9e4de49afca6e58a95c5
vovkpavlik <vovk.pavlik@bk.ru>
2020-12-16 19:57:24 +0300

    Доработал код по мелочи.

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import requests
 import random
 from environs import Env
-import pprint
-env = Env()
-env.read_env()
 
-key_pixabay = env.str("API_PIXABAY")
 
 def get_photo_url():
     tag = ["sportcars",
@@ -18,15 +14,14 @@ def get_photo_url():
             "winter",
             "buildings",
             "russia",
-            "bears"
-            ]
+            "bears"]
     random_tag = random.choice(tag)
-    params = {
+    parameters = {
         "key": key_pixabay,
         "q": random_tag,
     }
     url = "https://pixabay.com/api/"
-    random_photos = requests.get(url, params=params)
+    random_photos = requests.get(url, params=parameters)
     photo_urls = [pic["fullHDURL"] for pic in random_photos.json()["hits"]]
     random_photos.raise_for_status()
     return random.choice(photo_urls)
@@ -35,12 +30,12 @@ def get_photo_url():
 def send_photo():
     bot_url = f"https://api.telegram.org/bot{token_tg}"
     parameters = {
-        "chat_id": "CHAT_ID_TG",
+        "chat_id": chat_id_tg,
         "photo": get_photo_url()
     }
-    photo_bot = requests.post(f"{bot_url}/sendPhoto", data=parameters)
-    photo_bot.raise_for_status()
-    return photo_bot
+    response = requests.post(f"{bot_url}/sendPhoto", data=parameters)
+    response.raise_for_status()
+    return response
 
 
 if __name__ == "__main__":
@@ -49,8 +44,9 @@ if __name__ == "__main__":
 
     key_pixabay = env.str("API_PIXABAY")
     token_tg = env.str("TOKEN_TG")
+    chat_id_tg = env.str("CHAT_ID_TG")
 
     try:
         send_photo()
-    except requests.exceptions.HTTPError:
-        print("Непредвиденная ошибка")
+    except requests.exceptions.HTTPError as err:
+        print(err)
